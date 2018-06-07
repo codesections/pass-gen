404b5258b970ef54ab26e51b1157c60078095746
Daniel Sockwell <daniel@codesections.com>
2018-06-07 08:10:51 -0400

    Update man page

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 install -m 0644 -v ./man/pass-gen.1 /usr/share/man/man1/pass-gen.1
+install -m 0755 -v ./pass-gen /usr/bin/pass-gen
+install -d /usr/lib/pass-gen
+cp -r ./character-lists /usr/lib/pass-gen
+cp -r ./wordlists /usr/lib/pass-gen
