c4fb1af274ba12bc4306e16dd91d4a9e45c2f7d4
Daniel Sockwell <daniel@codesections.com>
2018-06-08 14:57:20 -0400

    Bugfix for uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -f /usr/bin/pass-gen
-rm -rf /usr/lib/pass-gen
-rm -f /usr/share/man/man1/pass-gen.1
+rm -f /usr/local/bin/pass-gen
+rm -rf /usr/local/lib/pass-gen
+rm -f /usr/local/share/man/man1/pass-gen.1
